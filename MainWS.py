4c21784173eae30a357c27a01249c020ace25de9
Kamil Hajduk <61657745+KamiiRu00@users.noreply.github.com>
2023-12-04 19:26:32 +0100

    chore: moved functions to util file

--- MainWS.py
+++ MainWS.py
@@ -1,56 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-
+from src.utilities import scrape_url_and_write_csv
 
 def main():
     OUTPUT_DIRECTORY = "output"
     for i in range(10):
         output_path = OUTPUT_DIRECTORY + f"\{i}.csv"
-        scrape_url(i,output_path)
+        url = create_url(i) 
+        scrape_url_and_write_csv(output_path,url)
 
 def create_url(number):
     return f"https://moodo.pl/pol_m_KOLEKCJA-192.html?counter={number}"
     
-
-def scrape_url(number,output_path):
-    response = requests.get(create_url(number))
-    soup = BeautifulSoup(response.text, 'html.parser')
-    data = soup.select('div[class="product col-6 col-sm-4 col-md-3 pt-3 pb-md-3 px-1 px-sm-3 px-md-4"]')
-    #print(data)
-
-
-    for i, x in enumerate(data):
-        print(f"Element {i+1}: {x.text}")
-
-    print(f"Total number of elements: {len(data)}")
-
-    # Define keywords for types of clothing
-    clothing_keywords = ['Spódnica', 'Koszulka', 'Spodnie',
-    'Sukienka', 'Sweter', 'Bluzka', 'Cienki sweter', 'Jeansy', 'Koszula']
-
-    # Cleaned data storage
-    cleaned_data = []
-
-    for i, x in enumerate(data):
-        product_info = x.text.strip()
-        
-        # Check for clothing keywords in product_info
-        clothing_type = next((keyword for keyword in clothing_keywords if keyword in product_info), 'Other')
-        
-        cleaned_data.append((i+1, clothing_type))
-
-    # Write cleaned data to a CSV file
-    with open(output_path, 'w', newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        
-        # Write header
-        csv_writer.writerow(['Index', 'Product Type'])
-        
-        # Write data
-        csv_writer.writerows(cleaned_data)
-
-    print(f"Cleaned data has been saved to {output_path}")
 
 if __name__ == "__main__":
     main()
---
+++ src/utilities.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+def scrape_url_and_write_csv(output_path,url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.find('div', class_="product__sizes mb-1"))
+    data = soup.select('div[class="product col-6 col-sm-4 col-md-3 pt-3 pb-md-3 px-1 px-sm-3 px-md-4"]')
+    #print(data)
+
+
+    for i, x in enumerate(data):
+        print(f"Element {i+1}: {x.text}")
+
+    print(f"Total number of elements: {len(data)}")
+
+    # Define keywords for types of clothing
+    clothing_keywords = ['Spódnica', 'Koszulka', 'Spodnie',
+    'Sukienka', 'Sweter', 'Bluzka', 'Cienki sweter', 'Jeansy', 'Koszula']
+
+    # Cleaned data storage
+    cleaned_data = []
+
+    for i, x in enumerate(data):
+        product_info = x.text.strip()
+        
+        # Check for clothing keywords in product_info
+        clothing_type = next((keyword for keyword in clothing_keywords if keyword in product_info), 'Other')
+        
+        cleaned_data.append((i+1, clothing_type))
+
+    write_csv(output_path, cleaned_data)
+
+
+def write_csv(output_path, cleaned_data):
+    # Write cleaned data to a CSV file
+    with open(output_path, 'w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        
+        # Write header
+        csv_writer.writerow(['Index', 'Product Type'])
+        
+        # Write data
+        csv_writer.writerows(cleaned_data)
+
+    print(f"Cleaned data has been saved to {output_path}")
