9b95cca9d10c774afa8145c679576d72f9f0c677
Kamil Hajduk <61657745+KHajduk00@users.noreply.github.com>
2024-01-10 17:12:14 +0200

    feat: merging data into one file for later plotting (#26)
    * made changes in both utilities and MainWS files that allowed us to merge csv's and then append the resulting file
    
    * updated the gitignore
    
    * adressed PR comments
    
    ---------
    
    Co-authored-by: Kamil Hajduk <61657745+KamiiRu00@users.noreply.github.com>

--- MainWS.py
+++ MainWS.py
@@ -1,18 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-from src.utilities import scrape_url_and_write_csv
+from src.utilities import scrape_url_and_write_csv, merge_csv_files
 
 def main():
     OUTPUT_DIRECTORY = "output"
+    OUTPUT_FOLDER_NAME = "single_csv_output"
+    OUTPUT_FILE_NAME = "merged_data.csv"
     for i in range(10):
         output_path = OUTPUT_DIRECTORY + f"\{i}.csv"
         url = create_url(i) 
         scrape_url_and_write_csv(output_path,url)
+    merge_csv_files(OUTPUT_DIRECTORY, OUTPUT_FOLDER_NAME, OUTPUT_FILE_NAME)
 
 def create_url(number):
     return f"https://moodo.pl/pol_m_KOLEKCJA-192.html?counter={number}"
     
-
 if __name__ == "__main__":
     main()
